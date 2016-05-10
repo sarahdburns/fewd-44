258fdaa66f5a16d88ab7446b6213be60f4f74632
sarahdburns <sarah.burns@prod-dmz-lb-vip-1001.opower.com>
2016-05-10 15:01:53 -0700

    Class 2 commit
    -travel news
    -wendy bit starter

---
+++ travel-news/js/travel-news.js
@@ -0,0 +1,7 @@
+var parent = document.querySelector(".sponsored-articles");
+
+var child =document.createElement("li");
+
+parent.appendChild(child);
+
+child.textContent="JS language is confusing!!!!";
