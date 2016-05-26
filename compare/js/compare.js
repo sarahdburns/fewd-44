9ef526ab310a441650ec5c666cf67ce7d0d37771
sarahdburns <sarah.burns@sburns.local>
2016-05-26 15:35:04 -0700

    Class 6 updates
    -fashion blog
    -compare code along (js)

--- compare/js/compare.js
+++ compare/js/compare.js
@@ -6,9 +6,47 @@ var sign = document.querySelector(".sign");
 var compare = document.querySelector(".compare");
 
 // Events
-compare.addEventListener('click', compareNumbers);
+compare.addEventListener("click", compareNumbers);
 
 //Event Handlers
 function compareNumbers () {
 	sign.textContent = "="
 }
+
+
+// Listen for the compare button to be clicked
+// compare numbers when click happens 
+compare.addEventListener("click",compareNumbers);
+
+
+//Event Handlers
+//-----------------------------------------------
+
+// compare the first and second number
+function compareNumbers () {
+
+	var firstValue = firstNumber.value;
+	firstValue = parseInt(firstValue);
+
+	var secondValue = secondNumber.value;
+	secondValue = parseInt(secondValue);
+
+	// update the page - change the sign
+	
+	//JS Conditional (if, else if, else)
+
+	//JS Comparison (greater than, less than)
+	if (firstValue > secondValue) {
+		sign.textContent = ">";
+
+	}	else if (firstValue < secondValue) {
+		sign.textContent = "<";
+	}
+		else if (firstValue == secondValue) {
+		sign.textContent = "=";
+	}
+
+}
+
+
+
