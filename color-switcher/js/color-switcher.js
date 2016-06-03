0293e451d0f08318e6a5e1987a33070c1b012739
sarahdburns <sarah.burns@sburns.local>
2016-06-02 19:59:21 -0700

    Color theme switcher
    -code along complete (changing states)

--- color-switcher/js/color-switcher.js
+++ color-switcher/js/color-switcher.js
@@ -5,6 +5,39 @@ var aliceblue = document.querySelector(".aliceblue");
 var rebeccapurple = document.querySelector(".rebeccapurple");
 var lightseagreen = document.querySelector(".lightseagreen");
 
+var textTheme = document.querySelector(".text-theme")
+
 function bodyColorChanger(color) {
 	body.className = color;
+}
+
+// Events 
+// ----------------------------------------------
+coral.addEventListener('click', changeThemeCoral);
+aliceblue.addEventListener('click', changeThemeAliceblue);
+rebeccapurple.addEventListener('click', changeThemeRebeccapurple);
+lightseagreen.addEventListener('click', changeThemeLightseagreen);
+
+function changeThemeCoral () {
+	console.log('fn changeThemeCoral');
+	body.className = "coral"; 
+	textTheme.textContent = "Coral";
+}
+
+function changeThemeAliceblue () {
+	console.log('fn changeThemeAliceblue');
+	body.className = "aliceblue"; 
+	textTheme.textContent = "Alice blue";
+}
+
+function changeThemeRebeccapurple () {
+	console.log('fn changeThemeRebeccapurple');
+	body.className = "rebeccapurple"; 
+	textTheme.textContent = "Rebecca purple";
+}
+
+function changeThemeLightseagreen () {
+	console.log('fn changeThemeLightseagreen');
+	body.className = "lightseagreen"; 
+	textTheme.textContent = "Light sea green";
 }
